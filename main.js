70a58f6e85cabff3b1e2139ebf409bcfd791f67b
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2022-11-12 16:45:53 -0500

    V2

--- main.js
+++ main.js
@@ -20,46 +20,112 @@ const bottonNine = document.querySelector('#button9');
 const bottonCero = document.querySelector('#button0');
 //bottonCero.value = 0;
 const suma = document.querySelector('#mas-button');
-const num1 = document.querySelector('#box-numbers');
-const numero1 = 0;
+const resta = document.querySelector('#menos-button');
+const multiplcacion = document.querySelector('#x-button');
+const division = document.querySelector('#divisor-button');
+const num = document.querySelector('#box-numbers');
+const igual = document.querySelector('#equal-button');
+const C = document.querySelector('#C-button');
+const CE = document.querySelector('#CE-button');
+let numero1;
+let numero2;
+let operation;
 
 
+//Eventos
 bottonOne.onclick = function(event1) {
-  num1.innerHTML +=bottonOne.value;
+  num.innerHTML +=bottonOne.value;
 
 }
 
 bottonTwo.onclick = function(event2) {
-  num1.innerHTML +=bottonTwo.value;
+  num.innerHTML +=bottonTwo.value;
 }
 
 bottonThree.onclick = function(event3) {
-  num1.innerHTML +=bottonThree.value;
+  num.innerHTML +=bottonThree.value;
 }
 bottonFour.onclick = function(event4) {
-  num1.innerHTML +=bottonFour.value;
+  num.innerHTML +=bottonFour.value;
 }
 bottonFive.onclick = function(event5) {
-  num1.innerHTML +=bottonFive.value;
+  num.innerHTML +=bottonFive.value;
 }
 bottonSix.onclick = function(event6) {
-  num1.innerHTML +=bottonSix.value;
+  num.innerHTML +=bottonSix.value;
 }
 bottonSeven.onclick = function(event7) {
-  num1.innerHTML +=bottonSeven.value;
+  num.innerHTML +=bottonSeven.value;
 }
 bottonEight.onclick = function(event8) {
-  num1.innerHTML +=bottonEight.value;
+  num.innerHTML +=bottonEight.value;
 }
 bottonNine.onclick = function(event9) {
-  num1.innerHTML +=bottonNine.value;
+  num.innerHTML +=bottonNine.value;
 }
 
 bottonCero.onclick = function(event9) {
-  num1.innerHTML +=bottonCero.value;
+  num.innerHTML +=bottonCero.value;
 }
 
 suma.onclick = function(event10) {
-  numero1 = num1.textContent;
+  numero1 = parseInt(num.innerHTML);
+  numero2 = parseInt(num.innerHTML);
+  num.innerHTML = '';
+  operation = "+";
 }
+
+resta.onclick = function(event11) {
+  numero1 = parseInt(num.innerHTML);
+  num.innerHTML = '';
+  operation = "-";
+}
+
+multiplcacion.onclick = function(event12) {
+  numero1 = parseInt(num.innerHTML);
+  num.innerHTML = '';
+  operation = "*";
+}
+
+division.onclick = function(event13) {
+  numero1 = parseInt(num.innerHTML);
+  num.innerHTML = '';
+  operation = "/";
+}
+
+igual.onclick = function(event14) {
+  numero2 = parseInt(num.innerHTML);
+  resolver(); 
+}
+
+C.onclick = function(event15) {
+  num.innerHTML = '';
+}
+
+CE.onclick = function(event16) {
+  num.innerHTML = '';
+  numero2 = parseInt(num.innerHTML);
+}
+
+// Funciones
+
+function resolver() {
+  let result;
+
+  if (operation === "+") {
+    result = numero1 + numero2;
+    num.innerHTML = result;
+    
+  } else if (operation === "-") {
+    result = numero1 - numero2;
+    num.innerHTML = result;
+  } else if (operation === "*") {
+    result = numero1 * numero2;
+    num.innerHTML = result;
+  } else {
+    result = numero1 / numero2;
+    num.innerHTML = result;
+  }
+}
+
 }
