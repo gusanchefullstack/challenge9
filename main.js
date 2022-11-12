028ea80f3f7cd62ea93c56182fb54bdae87a1af8
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2022-11-12 12:05:45 -0500

    V1

--- main.js
+++ main.js
@@ -1,48 +1,65 @@
 function init() {
-const bottonOne = document.getElementsByClassName('1-button');
+let bottonOne = document.querySelector('#button1');
 bottonOne.value = 1;
-const bottonTwo = document.getElementsByClassName('2-button');
+const bottonTwo = document.querySelector('#button2');
 //bottonTwo.value = 2;
-const bottonThree = document.getElementsByClassName('3-button');
+const bottonThree = document.querySelector('#button3');
 //bottonThree.value = 3;
-const bottonFour = document.getElementsByClassName('4-button');
+const bottonFour = document.querySelector('#button4');
 //bottonFour.value = 4;
-const bottonFive = document.getElementsByClassName('5-button');
+const bottonFive = document.querySelector('#button5');
 //bottonFive.value = 5;
-const bottonSix = document.getElementsByClassName('6-button');
+const bottonSix = document.querySelector('#button6');
 //bottonSix.value = 6;
-const bottonSeven = document.getElementsByClassName('7-button');
+const bottonSeven = document.querySelector('#button7');
 //bottonSeven.value = 7;
-const bottonEight = document.getElementsByClassName('8-button');
+const bottonEight = document.querySelector('#button8');
 //bottonEight.value = 8;
-const bottonNine = document.getElementsByClassName('9-button');
+const bottonNine = document.querySelector('#button9');
 //bottonNine.value = 9;
-const bottonCero = document.getElementsByClassName('0-button');
+const bottonCero = document.querySelector('#button0');
 //bottonCero.value = 0;
-const screen = document.getElementsByClassName('operations-container');
+const suma = document.querySelector('#mas-button');
+const num1 = document.querySelector('#box-numbers');
+const numero1 = 0;
 
-console.log(bottonOne);
 
-bottonOne.onclick = function(event) {
-  screen.textContent = bottonOne.value;
+bottonOne.onclick = function(event1) {
+  num1.innerHTML +=bottonOne.value;
+
 }
-console.log(screen);
+
+bottonTwo.onclick = function(event2) {
+  num1.innerHTML +=bottonTwo.value;
 }
 
+bottonThree.onclick = function(event3) {
+  num1.innerHTML +=bottonThree.value;
+}
+bottonFour.onclick = function(event4) {
+  num1.innerHTML +=bottonFour.value;
+}
+bottonFive.onclick = function(event5) {
+  num1.innerHTML +=bottonFive.value;
+}
+bottonSix.onclick = function(event6) {
+  num1.innerHTML +=bottonSix.value;
+}
+bottonSeven.onclick = function(event7) {
+  num1.innerHTML +=bottonSeven.value;
+}
+bottonEight.onclick = function(event8) {
+  num1.innerHTML +=bottonEight.value;
+}
+bottonNine.onclick = function(event9) {
+  num1.innerHTML +=bottonNine.value;
+}
 
+bottonCero.onclick = function(event9) {
+  num1.innerHTML +=bottonCero.value;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+suma.onclick = function(event10) {
+  numero1 = num1.textContent;
+}
+}
