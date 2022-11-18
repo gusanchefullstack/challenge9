9489e597592d939467fa1f2d871240d25a8dbd47
Gustavo Sanchez <63672753+gusanchefullstack@users.noreply.github.com>
2022-11-18 14:13:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,82 @@
 function init() {
-  const numbersbuttons = document.querySelectorAll('#button-number');
-  const operationButtons = document.querySelectorAll('#operator');
-  const Equal = document.querySelector('#equal-button');
-  const CE = document.querySelector('#CE-button');
-  const C = document.querySelector('#C-button');
-  const previewOperation = document.querySelector('#view-operartion');
-  const boxNumber = document.querySelector('#box-numbers');
-  let numbers = [];
-  let number1;
-  let resultado = 0;
+  const numbersbuttons = document.querySelectorAll("#button-number");
+  const operationButtons = document.querySelectorAll("#operator");
+  const Equal = document.querySelector("#equal-button");
+  const CE = document.querySelector("#CE-button");
+  const C = document.querySelector("#C-button");
 
-numbersbuttons.forEach(button => {
-  button.onclick = function(event1) {
-   
-      boxNumber.innerText +=button.innerText;
-  
-  } 
- });
-  
-operationButtons.forEach(button => {
-  button.onclick = function(event2) {
-    numbers.push(boxNumber.innerText);
-    number1 = parseInt(numbers[0]);
-    boxNumber.innerText = '';
-    acumular(number1);
-    numbers.shift();
-    previewOperation.innerText = resultado + "+";
-   
-    //boxNumber.innerText =resultado;
-    console.log(resultado);
- 
-  
-    function acumular() {
-      resultado +=number1;
-  
-    }
-   
-  } 
-  
-}); 
+  let operand1 = 0;
+  let operand2 = 0;
+  let operation = "";
+  let digits = "";
+  let result = 0;
 
+  const previewDisplay = document.querySelector("#view-operation");
+  const boxNumber = document.querySelector("#box-numbers");
+  boxNumber.innerText = "0";
 
- 
-  /*C.onclick = function(event3) {
-    numbers[0],numbers[1] = boxNumber.innerText = '0';
+  numbersbuttons.forEach((numberButton) => {
+    numberButton.addEventListener("click", (evt) => {
+      digits += evt.target.innerText;
+      console.log(`Digits: ${digits}`);
+      boxNumber.innerText = digits;
+    });
+  });
+  operationButtons.forEach((opButton) => {
+    opButton.addEventListener("click", (evt) => {
+      operand1 = parseInt(digits);
+      digits = "";
+      operation = opButton.innerText;
+      previsualizarOperacion(operand1, operand2, operation, previewDisplay);
+    });
+  });
+  Equal.addEventListener("click", (evt) => {
+    operand2 = parseInt(digits);
+    result = executeOperation(operation, operand1, operand2);
+    previsualizarOperacion(operand1, operand2, operation, previewDisplay);
+    console.log(`Resultado: ${result}`);
+    boxNumber.innerText = result;
+  });
+}
+
+function executeOperation(op, op1, op2) {
+  result = 0;
+  switch (op) {
+    case "+":
+      result = sum(op1, op2);
+      break;
+    case "-":
+      result = dif(op1, op2);
+      break;
+    case "x":
+      result = mult(op1, op2);
+      break;
+    case "/":
+      result = div(op1, op2);
+      break;
+    default:
+      break;
   }
+  return result;
+}
 
-  CE.onclick = function(event4) {
-    numbers[1] = boxNumber.innerText = '';
-  }
-*/
+function previsualizarOperacion(operand1, operand2, operationText, display) {
+  console.log(`Operand1: ${operand1}`);
+    console.log(`Operand2: ${operand2}`);
+    console.log(`Operation: ${operationText}`);
+  display.innerText = `${operand1} ${operationText} ${operand2?operand2:""}`;
 
 }
-  
 
-
+function sum(a, b) {
+  return a + b;
+}
+function dif(a, b) {
+  return a - b;
+}
+function mult(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
+}
